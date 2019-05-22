95c2d832565653bee1eada9e6edb9bd94eb2dfc7
IgnacioParrague <ibparrague@miuandes.cl>
2019-05-22 11:09:21 -0400

    started from scratch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-
   end
 
-  def current_user
-	#complete this method
-  end
-
-  def is_user_logged_in?
-	#complete this method
-  	logged_in = false
-	if logged_in then true else redirect_to root_path end 
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :users
 
-  root :to => 'sessions#new'
+  root :to => 'pages#home'
 
   post '/login',   to: 'sessions#create', as: :log_in
   delete '/log_out' => 'sessions#destroy', as: :log_out
